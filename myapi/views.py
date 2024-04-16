0d87e67e92c7149e1209ac94c0b40ed7f49fea35
Viktorio135 <102095652+Viktorio135@users.noreply.github.com>
2024-04-17 00:17:02 +0200

    мяу 2

--- myapi/views.py
+++ myapi/views.py
@@ -1,17 +1,18 @@
-from django.shortcuts import render
 from django.http import JsonResponse
 from langchain.schema import HumanMessage, SystemMessage
 from langchain.chat_models.gigachat import GigaChat
+
+from config import code
+
 
 import json
 
 def get_answer(request):
     body_unicode = request.body.decode('utf-8')
     body_data = json.loads(body_unicode)
-        
     text = body_data.get('message')
     if text != None:
-        chat = GigaChat(credentials='YmFhOTAzMGMtNTg1ZS00N2Q5LWE1MDEtNzU3MjA4ZjQzZDQzOmFlOTIxOGMyLWJiOGEtNGI2Yy04YmE5LWU2YWQ2YzgzNDRjNA==', verify_ssl_certs=False)
+        chat = GigaChat(credentials=code, verify_ssl_certs=False)
         messages = [
             SystemMessage(
                 content="Ты обычный ИИ чат"
